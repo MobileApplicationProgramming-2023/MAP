393e5709cdb03670df94675ad8baa88f36b089a5
Hana Hardy <111383188+HanaHardy@users.noreply.github.com>
2024-01-13 19:55:31 +0300

    another firebase update in main
    initialization of firebase

--- restaurant_reservation/lib/main.dart
+++ restaurant_reservation/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:restaurant_reservation/firebase_options.dart';
 import 'view/account_details.dart';
 import 'view/restaurant_list_page.dart';
 import 'view/feedback_page.dart';
@@ -20,7 +21,7 @@ import 'model/restaurant_profile_model.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
