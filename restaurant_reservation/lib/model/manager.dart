a33c1fb31233dbba4aff9944166ca07aac6a4f2c
Akar Azeez <119156639+axcix@users.noreply.github.com>
2024-01-15 17:55:37 +0300

    Update manager.dart

--- restaurant_reservation/lib/model/manager.dart
+++ restaurant_reservation/lib/model/manager.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Manager {
   final String id;
   final String username;
@@ -8,21 +10,53 @@ class Manager {
     required this.username,
     required this.password,
   });
-  factory Manager.fromJson(Map<String, dynamic> json){
-    return Manager(id: json['id'] ?? '', username: json['username'] ?? '', password: json['password'] ?? '');
+
+  factory Manager.fromJson(Map<String, dynamic> json) {
+    return Manager(
+      id: json['id'] ?? '',
+      username: json['username'] ?? '',
+      password: json['password'] ?? '',
+    );
   }
-  Map<String, dynamic> toJson(){
-    return{
-      'id':id,
-      'username':username,
-      'password':password,
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'username': username,
+      'password': password,
     };
   }
-  factory Manager.fromMap(Map<String, dynamic> map){
+
+  factory Manager.fromMap(Map<String, dynamic> map) {
     return Manager(
       id: map['id'],
-       username: map['username'],
-        password: map['password']
-        );
+      username: map['username'],
+      password: map['password'],
+    );
+  }
+
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+
+  static Future<Manager?> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    
+      User? user = _auth.currentUser;
+      return Manager(
+        id: user?.uid ?? '',
+        username: user?.email ?? '',
+        password: '',
+      );
+    } catch (e) {
+      print('Error signing in: $e');
+      return null;
+    }
+  }
+
+  static Future<void> signOut() async {
+    await _auth.signOut();
   }
 }
